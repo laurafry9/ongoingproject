4963e59e89608574b07414d879caab334cedef4a
laurafry9 <laura.fry9@gmail.com>
2021-06-11 23:36:32 -0700

    Fixed JS bugs

--- project/src/script.js
+++ project/src/script.js
@@ -13,20 +13,18 @@ let day = days[now.getDay()];
 let hours = now.getHours();
 let minutes = now.getMinutes();
 dateDisplay.innerHTML = `${day} ${hours}:${minutes}`;
-
-function getSearchedCity() {
+function getSearchedCity(event) {
+  event.preventDefault();
   let cityValue = document.querySelector("#enter-city");
   let displayedCity = document.querySelector("#city-name-value");
-  displayedCity.innerHTML = `${cityValue}`;
+  displayedCity.innerHTML = `${cityValue.value}`;
+  let apiKey = "e11ebc4d827268b35ca3d430280dba81";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue.value}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
 }
-let city = document.querySelector("#enter-city");
+let city = document.querySelector("#main-form-city");
 city.addEventListener("submit", getSearchedCity);
-
-let apiKey = "e11ebc4d827268b35ca3d430280dba81";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 function displayWeather(response) {
-  let temperatureElement = document.querySelector("#searched-temp");
+  let temperatureElement = document.querySelector(".current-temp");
   temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}`;
 }
-
-axios.get(apiUrl).then(displayWeather);
