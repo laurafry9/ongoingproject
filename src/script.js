504439297ad9927698649b2e6748e621e90b18e4
laurafry9 <laura.fry9@gmail.com>
2021-06-22 17:39:52 -0700

    Fixed/updated/finished timestamp js

--- src/script.js
+++ src/script.js
@@ -48,7 +48,25 @@ seattle.addEventListener("click", popularCitySeattle);
 
 function formatDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = timestamp(response.data);
+  let hours = date.getHours();
+  if (hours < 10) {
+    minutes = `0${minutes}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayWeather(response) {
@@ -67,5 +85,5 @@ function displayWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   let dateElement = document.querySelector("#date-element");
-  dateElement.innerHTML = `Friday 05:00`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
